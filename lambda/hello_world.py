70476eb2218d87b3bd6a0850d1c3f0c7a39482f0
Thomas Dimnet <thomas.dimnet@gmail.com>
2021-10-23 16:03:58 +0200

    Create the first lambda function and upload it using the CDK

---
+++ lambda/hello_world.py
@@ -0,0 +1,13 @@
+import json
+
+
+def handler(event, context):
+    print("Hello, world!")
+
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'text/plain'
+        },
+        'body': 'Hello, World from the AWS CDK'
+    }
--- serveless_image_uploader/serveless_image_uploader_stack.py
+++ serveless_image_uploader/serveless_image_uploader_stack.py
@@ -1,10 +1,7 @@
-from aws_cdk import core as cdk
-
-# For consistency with other languages, `cdk` is the preferred import name for
-# the CDK's core module.  The following line also imports it as `core` for use
-# with examples from the CDK Developer's Guide, which are in the process of
-# being updated to use `cdk`.  You may delete this import if you don't need it.
-from aws_cdk import core
+from aws_cdk import (
+    core as cdk,
+    aws_lambda as _lambda
+)
 
 
 class ServelessImageUploaderStack(cdk.Stack):
@@ -13,3 +10,9 @@ class ServelessImageUploaderStack(cdk.Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+        hello_world_lambda = _lambda.Function(
+            self, 'HelloWorldHandler',
+            runtime=_lambda.Runtime.PYTHON_3_8,
+            code=_lambda.Code.from_asset('lambda'),
+            handler='hello_world.handler'
+        )
