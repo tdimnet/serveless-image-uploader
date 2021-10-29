13b26a9c36b442758b2160bc7717e77a81b486d8
Thomas Dimnet <thomas.dimnet@gmail.com>
2021-10-29 16:12:56 +0200

    Add libs and project bootstrap:
    - Add react-router
    - Create the pages for the project
    - Create a Button component

---
+++ front-app/src/components/Button/index.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Component = () => (
+    <button>
+        Click me
+    </button>
+)
+
+export default Component
---
+++ front-app/src/pages/Home/index.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Component = () => (
+    <div>
+        <h1>Home Page</h1>
+    </div>
+)
+
+export default Component
